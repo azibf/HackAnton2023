b4a06e85130b132e92f50cec196e2f07989fc900
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-08-08 02:02:33 +0300

    fix(list-events): sort events by time

--- backend/services/event-v2/app/routes.py
+++ backend/services/event-v2/app/routes.py
@@ -16,7 +16,18 @@ TIMEZONE = os.environ["TIMEZONE"] if "TIMEZONE" in os.environ else "Europe/Mosco
 def list_events():
     calendar = get_calendar(CALENDAR_URL)
     events = get_events_for_today(calendar, TIMEZONE)
-    return {"data": {"content": list(map(Event.from_icalendar, events))}}
+    return {
+        "data": {
+            "content": list(
+                map(
+                    Event.from_icalendar,
+                    sorted(
+                        events, key=lambda event: event.get("DTSTART").dt, reverse=True
+                    ),
+                )
+            )
+        }
+    }
 
 
 @router.get("/event/{event_id}")
