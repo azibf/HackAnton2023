e561e770edeb88bb414dcfaedd4c2695c7fd15ef
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-08-08 00:55:46 +0300

    fix(event-v2): create FastAPI server after discovery

--- backend/services/event-v2/app/main.py
+++ backend/services/event-v2/app/main.py
@@ -8,7 +8,6 @@ import py_eureka_client.eureka_client as eureka_client
 from .entities.event import Event
 from .controller.calendar import get_calendar
 
-app = FastAPI()
 EUREKA_SERVER = os.environ["EUREKA_SERVER"] if "EUREKA_SERVER" in os.environ else None
 if EUREKA_SERVER:
     eureka_client.init(
@@ -16,6 +15,7 @@ if EUREKA_SERVER:
         app_name=os.environ["APP_NAME"] if "APP_NAME" in os.environ else "event-v2",
         instance_port=8080,
     )
+app = FastAPI()
 
 CALENDAR_URL = os.environ["EVENT_CAL_URL"]
 TIMEZONE = os.environ["TIMEZONE"] if "TIMEZONE" in os.environ else "Europe/Moscow"
