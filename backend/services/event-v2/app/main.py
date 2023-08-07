9153f527af5ec98a1a894f83ef95e0575c1050b1
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-08-08 01:19:48 +0300

    refactor(event-v2): add API router

--- backend/services/event-v2/app/main.py
+++ backend/services/event-v2/app/main.py
@@ -1,12 +1,9 @@
-from pytz import timezone
 from fastapi import FastAPI
-from itertools import chain
 from contextlib import asynccontextmanager
 import os
-import datetime, recurring_ical_events
 import py_eureka_client.eureka_client as eureka_client
 
-from .entities.event import Event
+from .routes import router
 from .controller.calendar import get_calendar
 
 
@@ -28,16 +25,9 @@ async def lifespan(_: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(router, prefix="/api/v1")
 
 CALENDAR_URL = os.environ["EVENT_CAL_URL"]
-TIMEZONE = os.environ["TIMEZONE"] if "TIMEZONE" in os.environ else "Europe/Moscow"
-
-
-# Invalidates the cache for a calendar
-@app.post("/calendar/invalidate")
-def invalidate():
-    get_calendar(CALENDAR_URL, force_refresh=True)
-    return {"success": "OK"}
 
 
 @app.get("/health")
@@ -45,46 +35,8 @@ def health():
     return {"status": "UP"}
 
 
-@app.get("/event")
-def list_events():
-    calendar = get_calendar(CALENDAR_URL)
-    events = recurring_ical_events.of(calendar).between(
-        datetime.datetime.combine(
-            datetime.date.today(), datetime.time(0, 0), tzinfo=timezone(TIMEZONE)
-        ),
-        datetime.datetime.combine(
-            datetime.date.today(), datetime.time(23, 59), tzinfo=timezone(TIMEZONE)
-        ),
-    )
-    all_day_events = filter(
-        lambda event: not isinstance(event.get("DTSTART").dt, datetime.datetime), events
-    )
-    fixed_time_events = sorted(
-        filter(
-            lambda event: isinstance(event.get("DTSTART").dt, datetime.datetime), events
-        ),
-        key=lambda event: event.get("DTSTART").dt,
-    )
-    return list(
-        map(Event.from_icalendar, list(chain(all_day_events, fixed_time_events)))
-    )
-
-
-@app.get("/event/{event_id}")
-def get_event():
-    raise NotImplementedError
-
-
-@app.post("/event")
-def create_event():
-    raise NotImplementedError
-
-
-@app.post("/event/{event_id}")
-def update_event():
-    raise NotImplementedError
-
-
-@app.delete("/event/{event_id}")
-def delete_event():
-    raise NotImplementedError
+# Invalidates the cache for a calendar
+@app.post("/calendar/invalidate")
+def invalidate():
+    get_calendar(CALENDAR_URL, force_refresh=True)
+    return {"success": "OK"}
---
+++ backend/services/event-v2/app/routes.py
@@ -0,0 +1,59 @@
+from pytz import timezone
+from fastapi import APIRouter
+from itertools import chain
+import os
+import datetime, recurring_ical_events
+
+from .entities.event import Event
+from .controller.calendar import get_calendar
+
+router = APIRouter()
+
+
+CALENDAR_URL = os.environ["EVENT_CAL_URL"]
+TIMEZONE = os.environ["TIMEZONE"] if "TIMEZONE" in os.environ else "Europe/Moscow"
+
+
+@router.get("/event")
+def list_events():
+    calendar = get_calendar(CALENDAR_URL)
+    events = recurring_ical_events.of(calendar).between(
+        datetime.datetime.combine(
+            datetime.date.today(), datetime.time(0, 0), tzinfo=timezone(TIMEZONE)
+        ),
+        datetime.datetime.combine(
+            datetime.date.today(), datetime.time(23, 59), tzinfo=timezone(TIMEZONE)
+        ),
+    )
+    all_day_events = filter(
+        lambda event: not isinstance(event.get("DTSTART").dt, datetime.datetime), events
+    )
+    fixed_time_events = sorted(
+        filter(
+            lambda event: isinstance(event.get("DTSTART").dt, datetime.datetime), events
+        ),
+        key=lambda event: event.get("DTSTART").dt,
+    )
+    return list(
+        map(Event.from_icalendar, list(chain(all_day_events, fixed_time_events)))
+    )
+
+
+@router.get("/event/{event_id}")
+def get_event():
+    raise NotImplementedError
+
+
+@router.post("/event")
+def create_event():
+    raise NotImplementedError
+
+
+@router.post("/event/{event_id}")
+def update_event():
+    raise NotImplementedError
+
+
+@router.delete("/event/{event_id}")
+def delete_event():
+    raise NotImplementedError
